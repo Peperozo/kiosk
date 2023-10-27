b6b2c0fc2e135382443fbc3cb8db2f85d5f76561
dearel4416 <dearel4416@gmail.com>
2023-10-27 12:48:11 +0900

    상품 생성

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main( String... args ) {
 		Scanner sc = new Scanner( System.in );
+		new ManagerMain(); // 메뉴 초기 설정
+
 		while( true ) {
 			System.out.println();
 			System.out.println( "무엇을 실행 하시겠습니까?" );
