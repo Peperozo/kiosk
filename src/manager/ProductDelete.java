de58894222a7b3c1b983ac27f8fa41a6ca311b5b
dearel4416 <dearel4416@gmail.com>
2023-10-30 14:01:33 +0900

    삭제 기능 출력 오류 수정

--- src/manager/ProductDelete.java
+++ src/manager/ProductDelete.java
@@ -9,6 +9,10 @@ public class ProductDelete {
     static Scanner scan = new Scanner(System.in);
 
     public static void deleteMenu() { // 메뉴 삭제
+        if(ManagerMain.mainMenu.isEmpty() && ManagerMain.newMainMenu.isEmpty()){
+            System.out.println("삭제할 메뉴가 없습니다.");
+            return;
+        }
         ManagerMain.mainMenu.forEach((key, value) -> { // 메인 메뉴 출력
             System.out.printf("ID%d. %-20s | %s\n", key, value[0], value[1]);
         });
@@ -48,6 +52,7 @@ public class ProductDelete {
             });
         }
         System.out.print("삭제할 상품이 든 메뉴 ID를 선택하세요: ");
+        loop:
         while (true) {
             int menuToProduct = scan.nextInt();
             if (menuToProduct == 9000) { // 기존 메뉴 선택
@@ -66,26 +71,31 @@ public class ProductDelete {
                 for (int i = 0; i < ProductCreation.menuArrayList.size(); i++) {
                     if (ProductCreation.menuArrayList.get(i).getMenuID() == menuToProduct) { // 같은 id 값을 가지고 있는 menuArrayList의 newProductMap 출력
                         selectProduct(ProductCreation.menuArrayList.get(i).newProductMap);
-                        break;
+                        break loop;
                     }
                 }
             }
-            System.out.println("잘못된 메뉴 ID입니다. 다시 입력해주세요: ");
+            System.out.print("잘못된 메뉴 ID입니다. 다시 입력해주세요: ");
         }
     }
 
     private static void selectProduct(Map<Integer, Product> pl) { // 삭제할 상품 선택
+        if(pl.isEmpty()) {
+            System.out.println("삭제할 상품이 없습니다.");
+            return;
+        }
+        System.out.println();
         for (var entry : pl.entrySet()) {
             int key = entry.getKey();
             Product p = entry.getValue();
             System.out.printf("ID%d. %-20s | W %4.1f | %s\n", key, p.getName(), p.getPrice() / 1000.0, p.getDescription());
         }
-        System.out.print("\n삭제할 상품 ID를 입력해 주세요 : ");
+        System.out.print("삭제할 상품 ID를 입력해 주세요 : ");
         while(true) {
             int productToDelete = scan.nextInt();
-            System.out.println();
             if (pl.containsKey(productToDelete)) {
                 pl.remove(productToDelete); // 해당 키값 골라서 삭제
+                System.out.println("해당 상품이 삭제되었습니다.");
                 break;
             } else {
                 System.out.println("잘못된 상품 ID입니다. 다시 입력해주세요: ");
